dac6a1272199147dd3566c3dac4a452dac39250e
Drunk Russian Gun <drunkrussiangun@gmail.com>
2020-10-01 19:34:31 +0300

    Реализовал функции withoutFirst, withoutLast и map через foldl и foldr

--- Semester6/HelloWorld/src/HigherOrder.hs
+++ Semester6/HelloWorld/src/HigherOrder.hs
@@ -21,3 +21,21 @@ lastViaFoldl list = fromJust $ Lists.foldl list Nothing (\ _ next -> Just next)
 lastViaFoldr :: [a] -> a
 lastViaFoldr list = fromJust $ Lists.foldr list Nothing (\ next last -> Just $ fromMaybe next last)
 
+withoutFirstViaFoldl :: [a] -> [a]
+withoutFirstViaFoldl list = let
+	addElementIfNotFirst :: Maybe [a] -> a -> Maybe [a]
+	addElementIfNotFirst list element = case list of
+		Nothing -> Just []
+		Just listValue -> Just $ listValue ++ [element]
+	in fromJust $ Lists.foldl list Nothing addElementIfNotFirst
+
+withoutLastViaFoldr :: [a] -> [a]
+withoutLastViaFoldr list = let
+	addElementIfNotLast :: a -> Maybe [a] -> Maybe [a]
+	addElementIfNotLast element list = case list of
+		Nothing -> Just []
+		Just listValue -> Just (element : listValue)
+	in fromJust $ Lists.foldr list Nothing addElementIfNotLast
+
+mapViaFoldr :: [a] -> (a -> b) -> [b]
+mapViaFoldr list mapFunction = Lists.foldr list [] (\ element mappedList -> mapFunction element : mappedList)
